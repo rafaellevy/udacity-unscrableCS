b341e23264c702f1455740d8a029b20e17db21eb
rafaellevy <srlevy2@gmail.com>
2021-09-14 15:41:03 -0700

    first commit

---
+++ Task1.py
@@ -0,0 +1,37 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+callsList = set() 
+textsList = set()
+
+for call in calls:
+    callsList.add(call[0])
+    callsList.add(call[1])
+
+for text in texts:
+    textsList.add(text[0])
+    textsList.add(text[1])
+
+result = callsList | textsList
+
+print("There are " + str(len(result)) + " different telephone numbers in the records.")
+
+
+
+
+"""
+TASK 1:
+How many different telephone numbers are there in the records? 
+Print a message:
+"There are <count> different telephone numbers in the records."
+"""
---
+++ Task2.py
@@ -0,0 +1,34 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+times = {}
+
+for call in calls:
+    if call[0] in times.keys() and times[call[0]] < int(call[3]):
+        times[call[0]] = int(call[3])
+    else:
+        times[call[0]] = int(call[3])
+        
+print(times.values())
+
+print(max(times.values()))
+
+"""
+TASK 2: Which telephone number spent the longest time on the phone
+during the period? Don't forget that time spent answering a call is
+also time spent on the phone.
+Print a message:
+"<telephone number> spent the longest time, <total time> seconds, on the phone during 
+September 2016.".
+"""
+
